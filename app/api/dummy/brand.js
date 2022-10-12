9cfb0be8e851c84d99f71045eb8e32bb2d4d4eb4
Muhammad Irfan <41617528+MuhammadIrfan5@users.noreply.github.com>
2022-10-13 00:59:00 +0500

    Inital Commit

---
+++ app/api/dummy/brand.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'Enjoy Life',
+  desc: 'Enjoy life with live streaming',
+  prefix: 'Enjoy Life',
+  footerText: 'Enjoy Life All Rights Reserved 2018',
+  logoText: 'Enjoy Life',
+};
---
+++ app/api/dummy/dummyContents.js
@@ -0,0 +1,18 @@
+import avatarApi from '../images/avatars';
+const dummyContents = {
+  user: {
+    name: 'Muhammad Irfan',
+    title: 'Administrator',
+    avatar: avatarApi[7],
+    status: 'online'
+  },
+  text: {
+    title: 'Enjoy Life',
+    subtitle: 'Live Streaming Application.',
+    sentences: 'Enjoy life with live streaming.',
+    paragraph: 'Enjoy Life Paragraph',
+    date: 'Oct 10, 2022'
+  }
+};
+
+export default dummyContents;
